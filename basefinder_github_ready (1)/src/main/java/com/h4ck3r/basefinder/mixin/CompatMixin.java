974da04318efd49c1279e818d58bccbe7194fc3a
isajar93 <diazisajar93@gmail.com>
2026-06-05 09:36:05 -0500

    Delete basefinder_github_ready (1) directory

--- basefinder_github_ready (1)/src/main/java/com/h4ck3r/basefinder/mixin/CompatMixin.java
+++
@@ -1,9 +0,0 @@
-package com.h4ck3r.basefinder.mixin;
-
-import net.minecraft.client.render.WorldRenderer;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(WorldRenderer.class)
-public class CompatMixin {
-    // Mixin vacío para compatibilidad
-}
